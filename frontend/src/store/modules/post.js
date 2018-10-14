772d7dbac10f65f60dd55330fd898abc43e224f4
moonyoungJo <jomy9649@gmail.com>
2018-10-15 03:41:17 +0900

    Complete list page

--- frontend/src/store/modules/post.js
+++ frontend/src/store/modules/post.js
@@ -1,15 +1,22 @@
 import {createAction, handleActions} from 'redux-actions';
-
-import {Map} from 'immutable';
+import {Map, fromJS} from 'immutable';
 import {pender} from 'redux-pender';
+import * as api from 'lib/api';
 
-//action type
+const GET_POST = 'post/GET_POST';
 
-//action creators
+export const getPost = createAction(GET_POST, api.getPost);
 
-//initial state
-const initialState = Map({});
+const initialState = Map({
+  post: Map({})
+});
 
 export default handleActions({
-
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const {data: post} = action.payload;
+      return state.set('post', fromJS(post));
+    }
+  })
 }, initialState);
